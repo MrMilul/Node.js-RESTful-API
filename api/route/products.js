aeba6abced16bbdef731948a7d18bda6dae917e8
MrMilul <miladmizani@yahoo.com>
2020-12-30 11:22:04 +0200

    CRUD and error handling are done

--- api/route/products.js
+++ api/route/products.js
@@ -1,37 +1,62 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const Product = require('../models/product')
+
+
 
 router.get('/', (req, res, next)=>{
-   
-    res.status(200).json({
-        message:"Handling Get request to /products"
-    })
+   Product.find().exec().then(docs =>{
+       if(docs.length > 0){
+        res.status(200).json(docs)
+       }else{
+           res.status(500).json({
+               message: "No data in DB"
+           })
+       }
+
+   })
+   .catch(err=>{
+       res.status(500).json({
+           error:err
+       })
+   })
 })
 
 router.post('/', (req, res, next)=>{
-    const prodduct = {
-        name: req.body.name,
-        price: req.body.price
-    }
-    res.status(201).json({
-        message:"Handling Post request to /products",
-        createdProduct: prodduct
+    const product = new Product({
+        _id : mongoose.Types.ObjectId(),
+        name:req.body.name,
+        price:req.body.price
     })
+    product.save().
+    then(result=>{
+        console.log(result)
+        res.status(201).json({
+            message:"Handling Post request to /products",
+            createdProduct: product
+        })
+    }).
+    catch(err=>{console.log(err)})
+    
 })
 
 router.get('/:productId', (req, res, next)=>{
     const id = req.params.productId
-    if (id === "special"){
-        res.status(200).json({
-            message:"You descovered special ID", 
-            id: id
+    Product.findById(id).exec()
+    .then(doc =>{ 
+        console.log(doc)
+        if (doc){
+            res.status(200).json(doc)
+        }else{
+            res.status(404).json({message:"No valid entry founded for provided data!"})
+        }
+    })
+    .catch((err)=>{
+        res.status(500).json({
+            error: err
         })
-    }else{
-        res.status(200).json({
-            message:"you intered an ID", 
-            id: id
-        })
-    }
+    })
 })
 
 
@@ -46,8 +71,14 @@ router.patch('/:productId', (req, res, next)=>{
 
 router.delete('/:productId', (req, res, next)=>{
     const id = req.params.productId
-    res.status(200).json({
-        message:`You Deleted a product by this => ${id} id `, 
+    Product.remove({_id:id})
+    .exec().then(result=>{
+        res.status(200).json(result)
+    })
+    .catch(err =>{
+        res.status(500).json({
+            error: err
+        })
     })
 })
 
