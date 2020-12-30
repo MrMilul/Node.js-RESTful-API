c86a84e646edc4c7fbd81fb0e7a9b5cbc1bb8d2e
MrMilul <miladmizani@yahoo.com>
2020-12-30 11:21:12 +0200

    mongo Atlas is connected

--- app.js
+++ app.js
@@ -2,14 +2,28 @@ const express = require("express")
 const app = express()
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 
 const productRoutes = require('./api/route/products')
 const orderRoutes = require ('./api/route/orders')
 
+
+//using morgan as a logger 
 app.use(morgan('dev'))
+
+//parse pur body buy bady-parser (both urlencoded and json data)
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
 
+//setting up Database using MongoDB Atlas
+// please replace <password>, and <dbname> by given data from the Atlas
+mongoose.connect("mongodb+srv://mrmilul:milad123456@cluster0.fy4sm.mongodb.net/node-rest?retryWrites=true&w=majority",
+{ useNewUrlParser: true, useUnifiedTopology: true })
+
+//logger! Just make sure the db is connected
+mongoose.connection.once("open", ()=>{console.log("DB connected")})
+
+//handling the headers and CORS
 app.use((req, res, next)=>{
     res.header('Access-Control-Allow-Origin', '*')
     res.header('Access-Control-Allow-Headers', '*')
@@ -23,10 +37,12 @@ app.use((req, res, next)=>{
 
 
 
-
+//importing APIs
 app.use('/products', productRoutes)
 app.use('/orders', orderRoutes)
 
+
+//handling the 404 errors  
 app.use((req, res, next)=>{
     const error = new Error('Not Found')
     error.status = 404
